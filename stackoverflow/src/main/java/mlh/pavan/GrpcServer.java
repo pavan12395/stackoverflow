c87844598ff6928a39464540af0a2393817d76c6
Pavan Kongara <pavan.kongara@nurture.farm>
2023-09-26 16:46:20 +0530

    added log4j2.xml file

--- stackoverflow/src/main/java/mlh/pavan/GrpcServer.java
+++ stackoverflow/src/main/java/mlh/pavan/GrpcServer.java
@@ -15,6 +15,7 @@ public class GrpcServer {
 
     public static void main(String[] args) throws Exception
     {
+        logger.info("Hello world");
         Server server = ServerBuilder.forPort(PropertyReader.getInstance().getGrpcServerPort()).addService(new StackOverFlowService()).build();
         try
         {
