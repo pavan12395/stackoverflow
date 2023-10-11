89ea75801bc70744edf80ca299a7b66c367ed348
Pavan Kongara <pavan.kongara@nurture.farm>
2023-10-11 13:09:55 +0530

    added changes

--- WebSocketServer/database/db.js
+++ WebSocketServer/database/db.js
@@ -2,14 +2,6 @@ require("dotenv").config()
 const Sequelize = require("sequelize");
 const path = require("path")
 const {database,username,password,host,port} = require("../constants/constants")
-console.log("Reading Inside Sequelize");
-console.log("Database : "+database);
-console.log("Username : "+username);
-console.log("Password : "+password);
-console.log("host : "+host);
-console.log("port : "+port);
-console.log("Reading Inside Sequelize");
-
 const sequelize = new Sequelize(database,username,password,{
     host: host,
     port: port,
